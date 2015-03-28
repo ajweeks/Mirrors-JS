2ce35cecacc8db653b6e2e0ccc6d7027ae396bfe
AJ <ajweeks@shaw.ca>
2015-03-28 15:51:11 -0600

    Converge all files into main.js
    Improve resource rendering
    Improve mouse input

--- js/main.js
+++ js/main.js
@@ -1,69 +1,469 @@
+// Copyright AJ Weeks 2015 
+
 "use strict";
 
-var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) { window.setTimeout(callback, 1000 / 60); };
+var get = function (what) {
+    return document.getElementById(what);
+};
 
-var mouse = null,
-    canvas = null,
-    context = null,
-    game = null,
-    mouse = null,
+var Game = {};
+
+Game.launch = function () {
+    Game.version = 0.021;
     
-    init = function () {
-        this.canvas = document.createElement('canvas');
-        document.body.appendChild(this.canvas);
-        this.context = this.canvas.getContext('2d');
+    Game.init = function () {
         
-        this.mouse = {
-            ldown : false, // lmb is down
-            lup : false, // lmb was just released
-            x : 0,
-            y : 0,
-            handleMove : function (event) {
-                mouse.x = event.offsetX;
-                mouse.y = event.offsetY;
-            },
-            handleClick : function (down) {
-                if (down) {
-                    mouse.ldown = down;
-                    mouse.lup = false;
-                } else { // down is 
-                    if (mouse.ldown) { // mouse was just 
-                        mouse.lup = true;
-                    } else {
-                        mouse.lup = false;
-                    }
-                    mouse.ldown = false;
-                }
-            },
-            update : function () {
-                mouse.lup = false;
-            }
+        //-------------------//
+        //      IMAGES       //
+        //-------------------//
+        Game.types = {};
+        Game.images = {};// FIXME put images in a better place
+        Game.images.lasers = {};
+        
+        Game.images.blank = new Image();
+        Game.types.BLANK = 0;
+        
+        Game.images.mirror = new Image();
+        Game.images.mirror.src = "res/mirror.png";
+        Game.images.mirror.alt = "mirror";
+        Game.types.MIRROR = 1;
+        
+        Game.images.pointer = new Image();
+        Game.images.pointer.src = "res/pointer.png";
+        Game.images.pointer.alt = "pointer";
+        Game.types.POINTER = 2;
+        
+        Game.images.receptor = new Image();
+        Game.images.receptor.src = "res/receptor_white.png";
+        Game.images.receptor.alt = "receptor";
+        Game.types.RECEPTOR = 3;
+        
+        Game.images.lasers.red = new Image();
+        Game.images.lasers.red.src = "res/laser_redNE.png";
+        Game.images.lasers.red.alt = "red laser";
+        Game.types.RED = 0;
+        
+        Game.images.lasers.green = new Image();
+        Game.images.lasers.green.src = "res/laser_green.png";
+        Game.images.lasers.green.alt = "green laser";
+        Game.types.GREEN = 1;
+        
+        Game.images.lasers.blue = new Image();
+        Game.images.lasers.blue.src = "res/laser_blue.png";
+        Game.images.lasers.blue.alt = "blue laser";
+        Game.types.BLUE = 2;
+        
+        Game.NORTH = 0;
+        Game.EAST = 1;
+        Game.SOUTH = 2;
+        Game.WEST = 3;
+        
+        Game.NW = 0;
+        Game.NE = 1;
+        
+        Game.ticks = 0;
+        Game.fps = 60;
+        
+        Game.mobile = 0;
+//        if (Android | iPhone | iPod) Game.mobile = 1; // LATER add mobile support
+        
+        Game.clickStr = Game.mobile ? 'ontouchend' : 'onclick';
+        
+        Game.mousex = -1;
+        Game.mousey = -1;
+        Game.lmb = false;
+        Game.rmb = false;
+        
+        Game.keysdown = {};
+        
+        Game.bgCanvas = get('backgroundCanvas');
+        Game.bgCanvas.width = window.innerWidth;
+        Game.bgCanvas.height = window.innerHeight;
+
+        Game.gameBoardCanvas = get('gameBoardCanvas');
+        
+        document.onresize = function () {
+            Game.bgCanvas.width = document.innerWidth;
+            Game.bgCanvas.height = document.innerHeight;
+            Game.render(); // attempt to reduce flicker on resize
         };
         
+        Game.prefs = [];
         
-        this.canvas.addEventListener('mousemove', this.mouse.handleMove, false);
-        this.canvas.addEventListener('mousedown', function (event) { mouse.handleClick(true); }, false);
-        this.canvas.addEventListener('mouseup', function (event) { mouse.handleClick(false); }, false);
+        Game.defaultPrefs = function () {
+            Game.debug = 1; // RELEASE make 0
+            Game.prefs.warn = !Game.debug;
+        };
+        Game.defaultPrefs();
         
-        this.canvas.width = 480;
-        this.canvas.height = 544;
+        Game.Button = function (text, context) {
+            this.text = text;
+            this.context = context;
+            
+            this.update = function () {
+                
+            };
+            
+            this.render = function () {
+                this.context.fillStyle = "yellow";
+                this.context.fillRect(120, 120, 100, 160);
+            };
+        };
         
-        this.game = new Game(new MainMenu());
-    },
+        ////////////////////////////////////////////////////////////////////////////
+        //                               STATES                                   //
+        ////////////////////////////////////////////////////////////////////////////
+        
+        Game.MainMenuState = function (sm) {
+            // LATER implement main menu state
+            this.sm = sm;
+            
+            // Create button divs
+            
+            this.update = function () {
+                // immediately enter game state
+                if (Game.lmb) {
+                    Game.bgCanvas.style.visibility = "hidden";
+                    this.sm.enterState(new Game.GameState(this.sm));
+                }
+            };
+            
+            this.render = function () {
+                var context = Game.bgCanvas.getContext('2d');
+                context.fillStyle = "#143665";
+                context.fillRect(0, 0, Game.bgCanvas.width, Game.bgCanvas.height);
+                context.fillStyle = "#0155c7";
+                context.font = "bold small-caps 325px sans-serif";
+                context.fillText("Test", 100, 280);
+                context.fillStyle = "#0566ea";
+                context.font = "bold small-caps 42px sans-serif";
+                context.fillText("Test", 112, 81);
 
-    step = function () {
-        game.update(this.mouse);
+                context.save();
+                context.translate(32, 32);
+                context.rotate((Math.PI / 2) * Game.NORTH);
+                context.drawImage(Game.images.pointer, -32, -32);
+                context.restore();
+
+                context.save();
+                context.translate(96, 32);
+                context.rotate((Math.PI / 2) * Game.EAST);
+                context.drawImage(Game.images.mirror, -32, -32);
+                context.restore();
+            };
+            
+            this.enterPreviousState = function () {
+                //there are no previous states to enter!
+            };
+            
+            // Gets called when this becomes the current state again (after being hidden)
+            this.restore = function () {
+                Game.bgCanvas.style.visibility = "visible";
+            };
+            
+            this.destroy = function () {
+                
+            };
+        }; // end Game.MainMenuState
         
-        this.mouse.update();
+        Game.StateManager = function () {
+            this.states = [];
+            this.states.push(new Game.MainMenuState(this));
+            
+            this.update = function () {
+                this.states[this.states.length - 1].update();
+            };
+            
+            this.render = function () {
+                this.states[this.states.length - 1].render();
+            };
+            
+            this.enterState = function (state) {
+                this.states.push(state);
+                Game.lmb = false;
+            };
+            
+            this.enterPreviousState = function () {
+                if (this.states.length > 1) {
+                    this.states.pop();
+                    this.states[this.states.length - 1].restore();
+                }
+                Game.lmb = false;
+            };
+        }; // end Game.StateManager()
+        Game.sm = new Game.StateManager();
         
-        context.fillStyle = "#2a2a2a"; // off black
-        context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        Game.GameState = function (sm) {
+            Game.GameState.Tile = function (type, maxdir, size, init, onmouseup, update, render) {
+                Game.GameState.Tile.Laser = function (image) {
+                    this.image = image;
+                    
+                    this.render = function (context, x, y, dir, size) {
+                        Game.renderEntity(context, x, y, dir, size);
+                    };
+                }; // end Game.GameState.Tile.Laser()
+                
+                this.size = size;
+                this.type = type;
+                this.lasers = [];
+                this.dir = Game.NORTH;
+                this.maxdir = maxdir;
+                
+                if (init) this.init = init;
+                else {
+                    this.init = function () {};
+                }
+                
+                if (onmouseup) this.onmouseup = onmouseup;
+                else {
+                    // Default onmouseup function
+                    this.onmouseup = function () {
+                        if (Game.lmb) {
+                            this.dir += 1;
+                            if (this.dir > this.maxdir) this.dir = 0;
+                        } else if (Game.rmb) {
+                            // Do nothing by default on right click
+                        }
+                    };
+                }
+                
+                if (update) this.update = update;
+                else {
+                    this.update = function () {
+                        // Default update function here
+                    };
+                }
+                
+                if (render) this.render = render;
+                else this.render = function (context, x, y) {
+                    // Default render function
+                    
+                    for (var i = 0; i < this.lasers.length; i++) {
+                        this.lasers[i].render(context, x, y, this.dir);
+                    }
+                    
+                    var image = Game.images.blank;
+                    switch (this.type) {
+                        case Game.types.BLANK:
+                            image = Game.images.blank;
+                            break;
+                        case Game.types.MIRROR:
+                            image = Game.images.mirror;
+                            break;
+                        case Game.types.POINTER:
+                            if (this.on) image = Game.images.pointer;
+                            else image = Game.images.lasers.blue;
+                            break;
+                        case Game.types.RECEPTOR:
+                            image = Game.images.receptor;
+                            break;
+                    }
+                    
+                    Game.renderEntity(context, x, y, image, this.dir, this.size);
+                    
+                    // LATER implement debug overlay
+//                    var xx = Math.floor((x - this.size / 2) / this.size),
+//                        yy = Math.floor((y - this.size / 2) / this.size);
+//                    if (Game.debug && Game.mouseContains(xx, yy, this.size)) {
+//                        context.strokeStyle = "white";
+//                        context.font = "32px Consolas";
+//                        context.strokeText(x + " " + y, Game.mousex, Game.mousey);
+//                    }
+                };
+                
+                this.addLaser = function (laser) {
+                    this.lasers.push(laser);
+                };
+                
+                this.removeLaser = function (laser) {
+                    if (this.lasers.indexOf(laser) > -1) {
+                        this.lasers.pop();
+                    }
+                };
+                
+                this.removeAllLasers = function () {
+                    for (var laser in this.lasers) {
+                        this.lasers.pop();
+                    };
+                };
+
+                this.copy = function () {
+                    return new Game.GameState.Tile(this.type, this.maxdir, this.size, this.init, this.onmouseup, this.update, this.render);
+                };
+                
+                this.init();
+            }; // end Game.GameState.Tile()
+        
+
+            Game.GameState.Board = function (w, h) {
+                this.w = w;
+                this.h = h;
+                this.tiles = [w * h];
+                this.tileSize = 64;
+                
+                Game.GameState.Board.BlankTile = new Game.GameState.Tile(Game.types.BLANK, 0, this.tileSize, null /* init */, null /* onmouseup */, null /* update */, null /* render */);
+                Game.GameState.Board.MirrorTile = new Game.GameState.Tile(Game.types.MIRROR, 1, this.tileSize, null /* init */, null /* onmouseup */, null /* update */, null /* render */);
+                Game.GameState.Board.PointerTile = new Game.GameState.Tile(Game.types.POINTER, 3, this.tileSize, 
+                function () { /* init */
+                    this.on = true;
+                },
+                function () { /* onmouseup */
+                    if (Game.lmb) {
+                        this.dir += 1;
+                        if (this.dir > this.maxdir) this.dir = 0;
+                    } else if (Game.rmb) {
+                        this.on = !this.on;
+                    }   
+                }, 
+                null /* update */, 
+                null /* render */);
+                Game.GameState.Board.ReceptorTile = new Game.GameState.Tile(Game.types.RECEPTOR, 3, this.tileSize, null /* init */, null /* onmouseup */, null /* update */, null /* render */);
+                                                                         
+                for (var i = 0; i < w * h; i++) {
+                    if (i % 2 == 0) this.tiles[i] = Game.GameState.Board.MirrorTile.copy();
+                    else if (i % 3 == 0) this.tiles[i] = Game.GameState.Board.PointerTile.copy();
+                    else this.tiles[i] = Game.GameState.Board.ReceptorTile.copy();
+                }
+                
+                this.update = function () {
+                    for (var i = 0; i < w * h; i++) {
+                        this.tiles[i].update();
+                    }
+                    if (Game.lmb || Game.rmb) {
+                        var xx = Math.floor(Game.mousex / this.tileSize),
+                            yy = Math.floor(Game.mousey / this.tileSize);
+                        this.tiles[yy * this.w + xx].onmouseup();
+                    }
+                };
+
+                this.render = function () {
+                    var context = Game.gameBoardCanvas.getContext('2d');
+                    
+                    context.fillStyle = '#0a0a0a';
+                    context.fillRect(0, 0, this.w * this.tileSize, this.h * this.tileSize);
+                    
+                    context.strokeStyle = 'gray';
+                    
+                    for (var i = 0; i < w * h; i++) {
+                        context.strokeRect((i % w) * this.tileSize, Math.floor(i / w) * this.tileSize, this.tileSize, this.tileSize);
+                        this.tiles[i].render(context, (i % w) * this.tileSize + this.tileSize / 2, Math.floor(i / w) * this.tileSize + this.tileSize / 2);
+                    };
+                };
+            }; // end Game.GameState.Board
+            
+            this.sm = sm;
+            this.board = new Game.GameState.Board(10, 9);
+            
+            Game.gameBoardCanvas.style.visibility = "visible";
+            Game.gameBoardCanvas.width = this.board.w * this.board.tileSize;
+            Game.gameBoardCanvas.height = this.board.h * this.board.tileSize;
+            
+            this.update = function () {
+                if (Game.keysdown[27]) {
+                    this.enterPreviousState();
+                    return;
+                }
+                this.board.update();
+            };
+            
+            this.render = function () {
+                this.board.render();
+            };
+            
+            this.enterPreviousState = function () {
+                this.destroy();
+                this.sm.enterPreviousState();
+            };
+                        
+            // Gets called when this becomes the current state again (after being hidden)
+            this.restore = function () {
+                Game.gameBoardCanvas.style.visibility = "visible";
+            };
+            
+            // Gets called right before this state is deleted
+            this.destroy = function () {
+                Game.gameBoardCanvas.style.visibility = "hidden";
+            };
+            
+        }; // end Game.GameState()
+        
+        Game.renderEntity = function (context, x, y, image, dir, size) {
+            context.save();
+            context.translate(x, y);
+            context.rotate(dir * 90 * (Math.PI / 180));
+
+            context.drawImage(image, -size / 2, -size / 2);
+            context.restore();
+        };
+        
+        Game.update = function () {
+            Game.ticks += 1;
+            Game.sm.update();
+            Game.lmb = false;
+            Game.rmb = false;
+        };
+        
+        Game.render = function () {
+//            var context = Game.bgCanvas.getContext('2d');
+            Game.sm.render();
+        };
+        
+    }; // end Game.init()
     
-        game.render(this.context);
-        animate(step);
+    // Main loop
+    Game.loop = function () {
+        Game.update();
+        if (document.hasFocus() || Game.ticks % 5 === 0) {
+            Game.render();
+        }
+        setTimeout(Game.loop, 1000 / Game.fps);
     };
+    
+}; // end Game.launch()
+
+Game.launch();
+
+function rightClick(event) {
+    
+    return false;
+};
+
+function boardClick(event) {
+    Game.mousex = event.offsetX;
+    Game.mousey = event.offsetY;
+    if ("which" in event) {
+        if (event.which === 1) Game.lmb = true;
+        else if (event.which === 3) Game.rmb = true;
+    } else if ("button" in event) {
+        
+    }
+};
+
+function boardHover(event, inside) {
+    if (inside) {
+        Game.mousex = event.offsetX;
+        Game.mousey = event.offsetY;
+    } else {
+        Game.mousex = -1;
+        Game.mousey = -1;
+    }
+};
+
+function keyPressed(event, down) {
+    Game.keysdown[event.keyCode ? event.keyCode : event.which] = down;
+};
+
+window.onkeydown = function (event) { keyPressed(event, true); };
+window.onkeyup = function (event) { keyPressed(event, false); };
+
+window.onbeforeunload=function(event) {
+    if (Game.prefs.warn) {
+        if (typeof event == 'undefined') event = window.event;
+        if (event) event.returnValue='Are you sure you want to close Mirrors?';
+    }
+}
 
 window.onload = function () {
-    this.init();
-    animate(step);
+    Game.init();
+    Game.loop();
 };
